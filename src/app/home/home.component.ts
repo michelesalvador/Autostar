1edc7587e067329299c9b7b6dd7d7ecd520c9b2b
michelesalvador <bluelettrico@iol.it>
2019-12-20 03:11:33 +0100

    Ritocchi alla home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutoService } from '../auto.service';
 import { Auto } from '../model/auto';
 
@@ -11,11 +12,15 @@ export class HomeComponent implements OnInit {
 
 	vetrinaAuto:Auto[];
 
-	constructor( private autoService: AutoService ) { }
+	constructor( private autoService: AutoService, private router:Router ) { }
 
 	ngOnInit() {
 		this.autoService.vetrinaAuto(3).subscribe( json => {
 			this.vetrinaAuto = json as Auto[];
 		});
+	}
+
+	apriDettaglio(id:number) {
+		this.router.navigate(['auto',id]);
 	}
 }
